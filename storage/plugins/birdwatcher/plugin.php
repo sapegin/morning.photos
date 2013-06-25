9c5cc688a759b085f142497639563760abae40da
Artem Sapegin <artem@sapegin.ru>
2013-06-25 08:00:48 +0000

    Base typography.

--- storage/plugins/birdwatcher/plugin.php
+++ storage/plugins/birdwatcher/plugin.php
@@ -1,7 +1,6 @@
 <?php
 
 class Birdwatcher extends KokenPlugin {
-
 	function __construct()
 	{
 		$this->register_filter('site.output', 'awesomize');
@@ -19,6 +18,7 @@ class Birdwatcher extends KokenPlugin {
 	function process_page($html)
 	{
 		foreach ($html('.bw-page') as $page) {
+			$page = $this->process_html($page, 'process_html_page');
 			$page = $this->process_photos($page);
 			$page->removeClass('bw-page');
 		}
@@ -81,6 +81,12 @@ class Birdwatcher extends KokenPlugin {
 		$first = $entry->firstChild(true)->detach(true);
 
 		return $entry;
+	}
+
+	function process_html_page($html_str, $entry)
+	{
+		$html_str = $this->typo_process($html_str);
+		return $html_str;
 	}
 
 	function process_html_essay($html_str, $entry)
@@ -235,4 +241,138 @@ class Birdwatcher extends KokenPlugin {
 		}
 		return null;
 	}
+
+	function typo_backup_tags($s)
+	{
+		$this->typo_tags[] = $s[1];
+		return '<≈>';
+	}
+
+	function typo_restore_tags($s)
+	{
+		$this->typo_tags;
+		return array_shift($this->typo_tags);
+	}
+
+	/*
+	 * Обогащение типографики. Кавычки, тире и прочие знаки уже должны быть расставлены.
+	 */
+	function typo_process($s)
+	{
+		// Убиваем табуляцию
+		$s = str_replace("\t", '', $s);
+		
+		// Убиваем повторяющиеся пробелы
+		$s = preg_replace('% +%', ' ', $s);
+
+		// Исправляем неразрывные пробелы
+		$s = str_replace("\xA9", '&nbsp;', $s);
+		
+		// Сохраняем теги
+		$s = preg_replace_callback('%(<[^>]*>)%ums', array($this, 'typo_backup_tags'), $s);
+
+		// if (defined('WP_TYPOHELPER_DUMMY') && WP_TYPOHELPER_DUMMY) {
+			// $s = typo_process_lite($s);
+		// }
+
+		$search = array(
+			'№ ',			// Номер
+			'§ ',			// Параграф
+			' —',			// Тире
+			'и т. д.',
+			'и т. п.',
+		);
+		$replace = array(
+			'№&nbsp;',
+			'§&nbsp;',
+			'&nbsp;—',
+			'и&nbsp;т.&nbsp;д.',
+			'и&nbsp;т.&nbsp;п.',
+		);
+		$s = str_replace($search, $replace, $s);
+
+		// Год
+		$s = preg_replace('%(?<![0-9])([0-9]{4}) (г\.)%ui', '\\1&nbsp;\\2', $s);
+		
+		// Имена собственные
+		// $s = preg_replace('%(?<![а-яёА-ЯЁ])([гГ]|[гГ]р|[тТ]ов)\. ([А-ЯЁ])%u', '\\1.&nbsp;\\2', $s);
+		
+		// Инициалы
+		$s = preg_replace('%(?<![а-яёА-ЯЁ])((?:[А-ЯЁ]\. ){1,2}[А-ЯЁ][-а-яё]+)%u', '<nobr>\\1</nobr>', $s);
+		
+		// Слова через дефис
+		$s = preg_replace('%(?<![а-яё])((?:[а-яё]{1,2}(?:\-[а-яё]+))|(?:[а-яё]+(?:\-[а-яё]{1,2})))(?![а-яё])%ui', '<nobr>\\1</nobr>', $s);
+		
+		// Частицы
+		$s = preg_replace('% (ж|бы|б|же|ли|ль|либо|или)(?![а-яё])%ui', '&nbsp;\\1', $s);
+		
+		// Предлоги и союзы
+		$s = preg_replace('%(?<![а-яё])(а|в|во|вне|и|или|к|о|с|у|о|со|об|обо|от|ото|то|на|не|ни|но|из|изо|за|уж|на|по|под|подо|пред|предо|про|над|надо|как|без|безо|что|да|для|до|там|ещё|их|или|ко|меж|между|перед|передо|около|через|сквозь|для|при|я)\s%ui', '\\1&nbsp;', $s);
+
+		// Валюты
+		$s = preg_replace('%(\d) (\$|р\.|руб\.)%ui', '\\1&nbsp;\\2', $s);
+		
+		// Даты
+		$s = preg_replace('%(\d) (января|февраля|марта|апреля|мая|июня|июля|августа|сентября|ноября|декабря)%ui', '\\1&nbsp;\\2', $s);
+
+		// Восстанавливаем теги
+		$s = preg_replace_callback("/<≈>/u", array($this, 'typo_restore_tags'), $s);
+
+		// Последнее слово в абзаце
+		$s = preg_replace('%\s([а-яё]+[.!?\)]</p>)%ui', '&nbsp;\\1', $s);
+
+		return trim($s);
+	}
+
+	/*
+	 * Типографика в стиле Ворда. Для комментариев и прочего неконтролируемого текста.
+	 */
+	function typo_lite($s)
+	{
+		// Убиваем табуляцию
+		$s = str_replace("\t", '', $s);
+		
+		// Убиваем повторяющиеся пробелы
+		$s = preg_replace('% +%', ' ', $s);
+		
+		// Сохраняем теги
+		$s = preg_replace_callback('%(<[^>]*>)%ums', array($this, 'typo_backup_tags'), $s);
+
+		$s = $this->typo_process_lite($s);
+		
+		// Исправляем неразрывные пробелы
+		$s = str_replace("\xA9", '&nbsp;', $s);	
+
+		// Восстанавливаем теги
+		$s = preg_replace_callback("/<≈>/u", array($this, 'typo_restore_tags'), $s);
+		
+		return $s;
+	}
+
+	/*
+	 * Типографика в стиле Ворда: обработка
+	 */
+	function typo_process_lite($s)
+	{
+		// Кавычки
+		$s = preg_replace('%"([а-яёa-z<])%ui', '«\\1', $s);
+		$s = preg_replace('%([а-яёa-z>])"%ui', '\\1»', $s);
+		
+		// Тире
+		$s = str_replace('--', '—', $s);
+		$s = preg_replace('%(^|[> \xA0])-|—($| )%u', '\\1—\\2', $s);
+		
+		// Апостроф
+		$s = str_replace("'", '’', $s);
+
+		// Многоточие
+		$s = str_replace('...', '…', $s);
+
+		// Копирайт
+		$s = str_replace('(C)', '©', $s);
+		$s = str_replace('(c)', '©', $s);
+		
+		return $s;
+	}
+
 }
