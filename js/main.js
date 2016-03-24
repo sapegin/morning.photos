90533e1fa437eb286d2639ed19df5fef94ccda0e
Artem Sapegin <artem@sapegin.ru>
2016-03-24 21:54:11 +0100

    Tamia 2, tamia-build, Fledermaus.

---
+++ js/main.js
@@ -0,0 +1,11 @@
+import 'tamia';
+import 'tamia/lib/modules/rouble';
+import 'tamia/lib/modules/switcher';
+// import 'tamia/lib/modules/modal';
+import 'tamia/lib/modules/form';
+import 'tamia/lib/modules/table';
+import 'tamia/lib/modules/media';
+import 'tamia/lib/modules/list';
+import 'tamia/lib/modules/richtypo';
+
+import '../styles/index.styl';
