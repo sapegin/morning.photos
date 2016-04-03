1a889d0a40741fce60b5dea4aca12e32a38cc655
Artem Sapegin <artem@sapegin.ru>
2016-04-03 17:00:15 +0200

    Print text in travel report template.

--- templates/Report.jsx
+++ templates/Report.jsx
@@ -1,8 +1,12 @@
+import flow from 'lodash/flow';
 import PageWithTitle from './PageWithTitle';
 
 export default function($) {
 	return (
 		<PageWithTitle {...$} pageType="report">
+			<div class="text">
+				{flow($.typo, $.safe)($.content)}
+			</div>
 		</PageWithTitle>
 	);
 }
