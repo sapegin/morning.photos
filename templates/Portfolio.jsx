8d48b8eb4229f2ab909cada708b5dbb3d65b7f45
Artem Sapegin <artem@sapegin.ru>
2016-04-03 17:00:03 +0200

    Use photo-grid for albums list.

--- templates/Portfolio.jsx
+++ templates/Portfolio.jsx
@@ -4,12 +4,12 @@ import Photo from './components/Photo';
 export default function($) {
 	return (
 		<PageWithTitle {...$} pageType="portfolio">
-			<div class="albums">
+			<div class="photo-grid photo-grid_three-even">
 				{$.albums.map(album => (
-					<div class="albums__item album-thumbnail">
-						<a class="album-thumbnail__link link link_quoted" href={album.url}>
-							<Photo slug={album.cover} size="small" alt={album.title} class="album-thumbnail__img" />
-							<h2 class="album-thumbnail__title">
+					<div class="photo-grid__photo">
+						<a class="link link_quoted" href={album.url}>
+							<Photo slug={album.cover} size="small" alt={album.title} class="photo-grid__img" />
+							<h2 class="photo-grid__title">
 								<u>{album.title}</u>
 							</h2>
 						</a>
