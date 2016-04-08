a52dc925604456969a901f62b2877ea7a054d3f4
Artem Sapegin <artem@sapegin.ru>
2016-04-08 20:22:59 +0200

    Do not link current tag on a tag page.

--- templates/components/PostMeta.jsx
+++ templates/components/PostMeta.jsx
@@ -1,18 +1,17 @@
 import { Group } from 'fledermaus/lib/components';
 
-// TODO
-// <span class="entry-tags__tag is-active"><%= name %></span><%- comma %>
-
 export default ({
-	post,
+	post, tag,
 	dateToString, option,
 }) => (
 	<footer class="essay__meta">
 		<time class="essay__pubdate entry-pubdate" datetime={post.date}>{dateToString(post.date)}</time>
 
 		<Group glue=", " class="entry-tags" inline>
-			{post.tags.map(tag => (
-				<a href={`/blog/tags/${tag}`} class="entry-tags__tag link">{option('tagNames')[tag]}</a>
+			{post.tags.map(name => name === tag ? (
+				<span class="entry-tags__tag is-active">{option('tagNames')[name]}</span>
+			) : (
+				<a href={`/blog/tags/${name}`} class="entry-tags__tag link">{option('tagNames')[name]}</a>
 			))}
 		</Group>
 	</footer>
