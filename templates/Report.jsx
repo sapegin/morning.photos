9b8ab0a1b1a7cf0ea74dc81c3e4f3d2cf784f29a
Artem Sapegin <artem@sapegin.ru>
2016-04-21 16:12:13 +0200

    Add share buttons to travel reports.

--- templates/Report.jsx
+++ templates/Report.jsx
@@ -1,13 +1,18 @@
 import PageWithTitle from './PageWithTitle';
+import Share from './components/Share';
 
 export default function($) {
-	const { content } = $;
-	const { typo } = $;
+	const { content, title } = $;
+	const { typo, Script, __ } = $;
 	return (
 		<PageWithTitle {...$} pageType="report">
 			<div class="text">
 				{typo(content)}
 			</div>
+			<div class="l-triple-space">
+				<Share {...$} title={`${title} — photos by Artem Sapegin`} />
+			</div>
+			<Script src="/build/main.js" />
 		</PageWithTitle>
 	);
 }
