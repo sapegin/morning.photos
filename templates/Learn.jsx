c00965cecfe98e30565dcc77f72f0a9e20993c00
Artem Sapegin <artem@sapegin.ru>
2016-06-20 22:20:57 +0200

    Fix Learn page layout.

--- templates/Learn.jsx
+++ templates/Learn.jsx
@@ -8,60 +8,56 @@ export default function($) {
 		<PageWithTitle pageType="learn" {...$}>
 			<div class="l-row text">
 				<div class="l-half">
-					<div class="articles-list">
-						<ul class="articles-list__list">
-							<li class="articles-list__item">
-								<a href="/learn/reading" class="link">Книги о фотографии</a>
-								<div class="articles-list__description">Рецензии на прочитанные мной книги</div>
-								<div class="latest-books">
-									{books.slice(0, 5).map(book => {
-										const alt = `${book.author}. ${book.title}`;
-										return (
-											<a href={`/learn/reading#${book.id}`} title={alt} class="latest-books__item">
-												<img
-													src={`/images/books/${book.id}.jpg`}
-													alt={alt}
-													class="latest-books__img"
-												/>
-											</a>
-										);
-									})}
-								</div>
-							</li>
-							<li class="articles-list__item">
-								<a href="/learn/microstocks" class="link">Микростоки</a>
-								<div class="articles-list__description">Как продавать фотографии в интернете</div>
-							</li>
-							<li class="articles-list__item">
-								<a href="/learn/quotes" class="link">Цитаты о фотографии и искусстве</a>
-								<div class="articles-list__description">Фотографы, художники и писатели</div>
-							</li>
-							<li class="articles-list__item">
-								<a href="/learn/funquotes" class="link">Фотография где</a>
-								<div class="articles-list__description">Смешные цитаты с фотофорумов</div>
-							</li>
-							<li class="articles-list__item">
-								<a href="/subscribe" class="link">Рассылка сайта</a>
-								<div class="articles-list__description">
-									Фотографии, статьи, новые проекты
-								</div>
-							</li>
-						</ul>
-					</div>
+					<ul class="articles-list">
+						<li class="articles-list__item">
+							<a href="/learn/reading" class="link">Книги о фотографии</a>
+							<div class="articles-list__description">Рецензии на прочитанные мной книги</div>
+							<div class="latest-books">
+								{books.slice(0, 5).map(book => {
+									const alt = `${book.author}. ${book.title}`;
+									return (
+										<a href={`/learn/reading#${book.id}`} title={alt} class="latest-books__item">
+											<img
+												src={`/images/books/${book.id}.jpg`}
+												alt={alt}
+												class="latest-books__img"
+											/>
+										</a>
+									);
+								})}
+							</div>
+						</li>
+						<li class="articles-list__item">
+							<a href="/learn/microstocks" class="link">Микростоки</a>
+							<div class="articles-list__description">Как продавать фотографии в интернете</div>
+						</li>
+						<li class="articles-list__item">
+							<a href="/learn/quotes" class="link">Цитаты о фотографии и искусстве</a>
+							<div class="articles-list__description">Фотографы, художники и писатели</div>
+						</li>
+						<li class="articles-list__item">
+							<a href="/learn/funquotes" class="link">Фотография где</a>
+							<div class="articles-list__description">Смешные цитаты с фотофорумов</div>
+						</li>
+						<li class="articles-list__item">
+							<a href="/subscribe" class="link">Рассылка сайта</a>
+							<div class="articles-list__description">
+								Фотографии, статьи, новые проекты
+							</div>
+						</li>
+					</ul>
 				</div>
 				<div class="l-half">
-					<div class="articles-list">
-						<h2 class="articles-list__title">
-							<a href="/blog" class="link">Блог</a>
-						</h2>
-						<ul class="articles-list__list">
-							{importantPosts.map(post => (
-								<li class="articles-list__item">
-									<a href={post.url} class="link">{typo(post.title)}</a>
-								</li>
-							))}
-						</ul>
-					</div>
+					<h2 class="beta">
+						<a href="/blog" class="link">Блог</a>
+					</h2>
+					<ul class="articles-list">
+						{importantPosts.map(post => (
+							<li class="articles-list__item">
+								<a href={post.url} class="link">{typo(post.title)}</a>
+							</li>
+						))}
+					</ul>
 				</div>
 			</div>
 		</PageWithTitle>
