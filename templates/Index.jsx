c3fcfec8fa0c12ae61460c4a964c6dd5e810d741
Artem Sapegin <artem@sapegin.ru>
2016-06-26 21:58:38 +0200

    Responsiveness for the new home page.

--- templates/Index.jsx
+++ templates/Index.jsx
@@ -1,8 +1,11 @@
+import block from 'bem-cn';
 import Page from './Page';
 import ArticlesList from './components/ArticlesList';
 import Photo from './components/Photo';
 import SubscribeForm from './components/SubscribeForm';
 import { getPhotoUrl } from '../js/util/util';
+
+const b = block('index');
 
 export default function($) {
 	const { content, links, photoPosts, posts, userpic, subscribeNote } = $;
@@ -13,56 +16,54 @@ export default function($) {
 
 	return (
 		<Page {...$}>
-			<div class="index content">
-				<div class="l-quint-space">
-					<a href={photo1.url} class="index__primary-photo">
+			<div class={b.mix('content')}>
+				<div class={b('photos-section')}>
+					<a href={photo1.url} class={b('primary-photo').mix(['link', 'link_quoted'])}>
 						<Photo slug={photo1.firstPhoto} size="medium" />
-						<div class="index__primary-photo-title">
-							<span class="index__primary-photo-title-text">
+						<div class={b('primary-photo-title')}>
+							<u class={b('primary-photo-title-text')}>
 								{photo1.title}
-							</span>
+							</u>
 						</div>
 					</a>
 					{photo2 && photo3 &&
-						<div class="index__secondary-photos">
-							<a href={photo2.url} class="index__secondary-photo link link_quoted">
+						<div class={b('secondary-photos')}>
+							<a href={photo2.url} class={b('secondary-photo', { first: true }).mix(['link', 'link_quoted'])}>
 								<div
-									class="index__secondary-photo-img"
-									style={`background-image:url(${getPhotoUrl(photo2.firstPhoto, 'small')})`}
+									class={b('secondary-photo-img')}
+									style={`background-image:url(${getPhotoUrl(photo2.firstPhoto, 'medium')})`}
 								></div>
-								<u class="index__secondary-photo-title">{photo2.title}</u>
+								<div class={b('secondary-photo-title', { first: true })}>
+									<u>{photo2.title}</u>
+								</div>
 							</a>
-							<a href={photo3.url} class="index__secondary-photo link link_quoted">
+							<a href={photo3.url} class={b('secondary-photo', { last: true }).mix(['link', 'link_quoted'])}>
 								<div
-									class="index__secondary-photo-img"
-									style={`background-image:url(${getPhotoUrl(photo3.firstPhoto, 'small')})`}
+									class={b('secondary-photo-img')}
+									style={`background-image:url(${getPhotoUrl(photo3.firstPhoto, 'medium')})`}
 								></div>
-								<u class="index__secondary-photo-title">{photo3.title}</u>
+								<div class={b('secondary-photo-title', { last: true })}>
+									<u>{photo3.title}</u>
+								</div>
 							</a>
 						</div>
 					}
 				</div>
-				<div class="l-sext-space">
-					<div class="l-double-space">
-						<ArticlesList list={postsList} cols markImportant {...$} />
-					</div>
+				<div class={b('articles-section')}>
+					<ArticlesList list={postsList} cols markImportant {...$} />
 					{links &&
 						<ArticlesList list={links} cols {...$} />
 					}
 				</div>
-				<div class="index__about-container">
-					<div class="index__userpic">
-						<img
-							src={userpic}
-							alt={__('author')}
-							class="index-content__userpic-img"
-						/>
+				<div class={b('about-section')}>
+					<div class={b('userpic')}>
+						<img src={userpic} alt={__('author')} />
 					</div>
-					<div class="index__about">
-						<div class="index__about-text text">
+					<div class={b('about')}>
+						<div class={b('about-text').mix('text')}>
 							{typo(content)}
 						</div>
-						<div class="index__subscribe">
+						<div class={b('subscribe')}>
 							<SubscribeForm {...$} from="Index" />
 							<div class="note">{typo(subscribeNote)}</div>
 						</div>
