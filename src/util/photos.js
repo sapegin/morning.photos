50f4491cc5fc379b97bcf4d87e7dd57473e5dd5f
Artem Sapegin <asapegin@wayfair.com>
2019-01-22 15:52:00 +0100

    Enable retina images

--- src/util/photos.js
+++ src/util/photos.js
@@ -5,15 +5,15 @@ const PHOTO_PROTOCOL = 'photo://';
 const URL_PREFIX = 'https://res.cloudinary.com/morningphotos/image/upload';
 
 const SIZES = {
-	blog: 'c_scale,dpr_auto,f_auto,q_auto:best,e_sharpen:70,w_1024',
-	gallery: 'c_scale,dpr_auto,f_auto,q_auto:best,e_sharpen:70,w_1600',
-	thumbnail: 'c_scale,dpr_auto,f_auto,q_auto:good,w_600',
+	blog: 'dpr_auto,f_auto,q_auto:best,e_sharpen:70,w_1024,c_fit',
+	gallery: 'dpr_auto,f_auto,q_auto:best,e_sharpen:70,w_1600,h_1200,c_fit',
+	thumbnail: 'dpr_auto,f_auto,q_auto:good,w_600,c_fit',
 };
 
 const getAutoTranformation = width => {
 	const roundedWidth = 100 * Math.ceil(width / 100);
 	const quality = roundedWidth >= 500 ? 'best' : 'good';
-	return `c_scale,dpr_auto,f_auto,q_auto:${quality},w_${roundedWidth}`;
+	return `dpr_auto,f_auto,q_auto:${quality},w_${roundedWidth},c_fit`;
 };
 
 export type Size = $Keys<typeof SIZES>;
