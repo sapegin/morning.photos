c3785bb801c19cf850c072a3ce14e3f04fc4d421
Artem Sapegin <asapegin@wayfair.com>
2019-03-11 13:34:16 +0100

    Open newsletter folder after downloading photos

--- src/scripts/download-email-photos.js
+++ src/scripts/download-email-photos.js
@@ -1,10 +1,7 @@
-import path from 'path';
 import download from 'download';
 import { getImages, getPhotoNameFromUrl } from '../util/node';
 import { loadPhoto } from '../util/gallery';
 import { getPhotoUrl } from '../util/photos';
-
-const DOWNLOAD_DIR = path.resolve(__dirname, '../../newsletter');
 
 const handleError = title => err => {
 	// eslint-disable-next-line no-console
@@ -14,12 +11,12 @@ const handleError = title => err => {
 	process.exit(1);
 };
 
-export default async function downloadPhotos(markdown) {
+export default async function downloadPhotos(markdown, dir) {
 	const images = getImages(markdown);
 	const names = images.map(getPhotoNameFromUrl);
 	const photos = await Promise.all(names.map(loadPhoto)).catch(handleError('Error loading photos'));
 	const urls = photos.map(photo => getPhotoUrl(photo.name, photo.timestamp, 'email'));
-	await Promise.all(urls.map(url => download(url, DOWNLOAD_DIR))).catch(
+	await Promise.all(urls.map(url => download(url, dir))).catch(
 		handleError('Error downloading photos')
 	);
 }
