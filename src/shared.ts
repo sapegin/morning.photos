9aead4c89aebe4d8815c864c62dd09022ef29da8
Artem Sapegin <artem@sapegin.ru>
2025-05-29 10:49:34 +0200

    Gatsby → Astro

--- src/shared.ts
+++
@@ -1,7 +0,0 @@
-export const SITE_URL = 'https://morning.photos';
-export const SITE_TITLE = 'Artem Sapegin Photography';
-export const SITE_DESCRIPTION =
-	'Artem Sapegin Photography: landscapes, travel, cityscapes, nature, dogs';
-export const SITE_EMAIL = 'artem@sapegin.ru';
-export const SITE_TWITTER = '@iamsapegin';
-export const SITE_FB_APP_ID = '155027288234245';
