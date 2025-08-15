cc56e2ade03f99de8b7597d3bfcf81bd8f0771b5
Artem Sapegin <artem@sapegin.ru>
2025-08-15 17:14:40 +0200

    Update email

--- src/constants.ts
+++ src/constants.ts
@@ -5,7 +5,6 @@ export const SITE_TITLE = 'Artem Sapegin Photography';
 export const SITE_DESCRIPTION =
 	'Artem Sapegin Photography: landscapes, travel, cityscapes, nature, coffee';
 
-export const ME_EMAIL = 'artem@sapegin.ru';
 export const ME_MASTODON_URL = 'https://mastodon.cloud/@sapegin';
 export const ME_MASTODON_HANDLE = '@sapegin@mastodon.cloud';
 export const ME_BLUESKY_URL = 'https://bsky.app/profile/sapegin.me';
